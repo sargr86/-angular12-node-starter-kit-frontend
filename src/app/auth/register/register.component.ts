b6df62160affcbf383a75448a419087c33e9a87a
Grigor Sargsyan <sargr86@gmail.com>
2021-06-16 18:14:01 +0400

    Started registration form fields implementation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
+import {patternValidator} from '@core/helpers/pattern-validator';
+import {EMAIL_PATTERN} from '@core/constants/patterns';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  registrationForm: FormGroup;
+  subscriptions: Subscription[] = [];
+  isSubmitted = false;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    public router: Router
+  ) {
+    this.registrationForm = this.fb.group({
+      full_name: ['', Validators.required],
+      email: ['', [Validators.required, patternValidator(EMAIL_PATTERN)]],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  register(): void {
+
+  }
+
+  get fullName(): AbstractControl {
+    return this.registrationForm.controls.full_name;
+  }
+
+  get email(): AbstractControl {
+    return this.registrationForm.controls.email;
+  }
+
+  get pass(): AbstractControl {
+    return this.registrationForm.controls.password;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
 }
